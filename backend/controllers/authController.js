a270d19ff2e74df8931642c7995d6350c96ceeb7
sidhdhali <sidhnakrani1911@gmail.com>
2024-08-13 14:05:13 +0200

    message model

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -92,5 +92,14 @@ export const login = async (req, res) =>
 
 export const logout = (req, res) =>
 {
-  console.log("logout")
+  try
+  {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "logged out successfully" });
+  } catch (error)
+  {
+    console.log("Error in logout controller", error.message)
+    res.status(500).json({ error: "internal server error" });
+
+  }
 }
