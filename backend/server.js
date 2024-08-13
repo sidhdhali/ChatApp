e297fd5faa353651f849fcb1b1d73347b3a3e9c7
sidhdhali <sidhnakrani1911@gmail.com>
2024-08-13 14:37:16 +0200

    message controller

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js"
 import connection from "./db/connect.js";
 
 const app = express();
@@ -10,9 +12,11 @@ const PORT = process.env.port || 5000;
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming req with JSON payloads (frpm req.body)
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () =>
 {
