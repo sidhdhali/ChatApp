e96ddf0f6c56d45eaf93d647f64fc140460ed52c
sidhdhali <sidhnakrani1911@gmail.com>
2024-08-20 10:43:00 +0200

    render url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -20,7 +20,7 @@ export const SocketContextProvider = ({ children }) =>
   {
     if (authUser)
     {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chatapp-fqop.onrender.com", {
         query: {
           userId: authUser._id,
         }
